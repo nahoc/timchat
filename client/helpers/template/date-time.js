95af2a5fb85f4093ae1748ddd5122e2790d682a1
Cohan <1464690@etu.cegep-ste-foy.qc.ca>
2017-01-26 19:16:06 -0500

    ajout du timestamp; todo: modifier pour canada

---
+++ client/helpers/template/date-time.js
@@ -0,0 +1,13 @@
+/*
+ ** Helper pour le timestamp d'un message
+ ** todo: modifier l'afficher pour Canadien Français
+ */
+Template.registerHelper('messageTimestamp', (timestamp) => {
+    if (timestamp) {
+        let today = moment().format('YYYY-MM-DD'),
+            datestamp = moment(timestamp).format('YYYY-MM-DD'),
+            isBeforeToday = moment(today).isAfter(datestamp),
+            format = isBeforeToday ? 'MMMM Do, YYYY hh:mm a' : 'hh:mm a';
+        return moment(timestamp).format(format);
+    }
+});
