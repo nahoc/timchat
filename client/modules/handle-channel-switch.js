1e4e025c3c235f28c9f62aab4c6428456470875b
Cohan <1464690@etu.cegep-ste-foy.qc.ca>
2017-03-07 14:48:44 -0500

    onglet affiche le bon titre

--- client/modules/handle-channel-switch.js
+++ client/modules/handle-channel-switch.js
@@ -1,10 +1,8 @@
 /*
  ** Script qui gère le switch entre les channels
  */
-
 // import
 import setScroll from './set-scroll';
-
 let _establishSubscription = (template, isDirect, channel) => {
     // subscribtion...
     template.subscribe('channel', isDirect, channel, () => {
@@ -15,11 +13,9 @@ let _establishSubscription = (template, isDirect, channel) => {
         // délai de 300ms
     });
 };
-
 let _handleSwitch = (template) => {
     // on obtient la valeur du channel dans l'URL
     let channel = FlowRouter.getParam('channel');
-
     if (channel) {
         // si le channel contient un "@" il s'agit d'un message privé
         let isDirect = channel.includes('@');
@@ -27,17 +23,21 @@ let _handleSwitch = (template) => {
         // le template se met en mode "loading"
         template.loading.set(true);
         _establishSubscription(template, isDirect, channel);
+        if (isDirect) {
+            document.title = channel + " - timCHAT";
+        }
+        else {
+            document.title = "#" + channel + " - timCHAT";
+        }
     }
 };
-
 // création des reactive variables
 let _setupReactiveVariables = (template) => {
     template.isDirect = new ReactiveVar();
     template.loading = new ReactiveVar(true);
 };
-
 // export
-export default function(template) {
+export default function (template) {
     _setupReactiveVariables(template);
     // chaque fois que L'URL change, cette fonction est exécutée
     Tracker.autorun(() => {
