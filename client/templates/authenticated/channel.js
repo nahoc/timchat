b2a75f738e362d2a1e45d554418957669f6daed6
Cohan <1464690@etu.cegep-ste-foy.qc.ca>
2017-05-15 15:22:05 -0400

    user is typing

--- client/templates/authenticated/channel.js
+++ client/templates/authenticated/channel.js
@@ -2,13 +2,23 @@
 import handleChannelSwitch from '../../modules/handle-channel-switch';
 import handleMessageInsert from '../../modules/handle-message-insert';
 import sortMessages from '../../modules/sort-messages';
+//import isTyping from '../../modules/is-typing';
 // après la création du template des channels...
 Template.channel.onCreated(() => {
     let template = Template.instance();
     // appel de la fonction handleChannelSwitch
     handleChannelSwitch(template);
 
+     Tracker.autorun(function (tracker) {
+          if (Meteor.userId() && Streamy.id()) {
+              Meteor.users.update({_id: Meteor.userId()}, {$set: {'profile.socketSession': Streamy.id()}});
+              tracker.stop();
+          }
+      });
+
 });
+
+
 // helpers
 Template.channel.helpers({
     // si on est en loading
@@ -45,9 +55,29 @@ Template.channel.helpers({
             }
         }
 });
+
+/* is typing */
+var typingTimer;
+var doneTypingInterval = 2000;
+function doneTyping() {
+    Streamy.broadcast('isNotTyping', { data: "" });
+}
+Streamy.on('isTyping', function(d) {
+    let user = Meteor.users.findOne({
+        _id: d.__fromUserId
+    });
+    $('#is-typing').html(user.username + " est en train d'écrire");
+});
+Streamy.on('isNotTyping', function(d) {
+    $('#is-typing').html("");
+});
+
 // events
 Template.channel.events({
     'keydown [name="message"]' (event, template) {
         handleMessageInsert(event, template);
+        Streamy.broadcast('isTyping', { data: "" });
+        clearTimeout(typingTimer);
+        typingTimer = setTimeout(doneTyping, doneTypingInterval);
     }
 });
